8eb44512ea12a310a038b1fa6af5b139f592a19a
vect0r <vectoreddd@gmail.com>
2024-05-15 00:41:34 +0200

    prototype version 2, added textures and shooting, reworked the code to an object oriented approach

--- main.cpp
+++ main.cpp
@@ -1,20 +1,149 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 #include <iostream>
+#include <vector>
+
+class Player : public sf::Sprite
+{
+public:
+    Player(sf::Vector2f position, sf::Vector2f size)
+    {
+        sf::Texture texture;
+        if(!texture.loadFromFile("player.png"))
+        {
+            std::cout << "Error loading player texture" << std::endl;
+        
+        }
+        
+        setScale(size);
+        
+        setPosition(position);
+    }
+
+    void movePlayer(sf::RenderWindow& window)
+    {
+        velocity.x = 0;
+        velocity.y = 0;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            velocity.x = -speed;
+            if (getPosition().x <= 0)
+            {
+                setPosition(0, getPosition().y);
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            velocity.x = speed;
+            if (getPosition().x + getScale().x >= window.getSize().x)
+            {
+                setPosition(window.getSize().x - getScale().x, getPosition().y);
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            velocity.y = -speed;
+            if (getPosition().y <= 0)
+            {
+                setPosition(getPosition().x, 0);
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            velocity.y = speed;
+            if (getPosition().y + getScale().y >= window.getSize().y)
+            {
+                setPosition(getPosition().x, window.getSize().y - getScale().y);
+            }
+        }
+    }
+
+    void update(float deltaTime)
+    {
+        move(velocity.x * deltaTime, velocity.y * deltaTime);
+    }
+
+    void draw(sf::RenderWindow& window)
+    {
+        window.draw(*this);
+    }
+
+private:
+    sf::Vector2f velocity;
+    float speed = 400.0f;
+};
+
+class Bullet : public sf::RectangleShape
+{
+public:
+    Bullet(sf::Vector2f position, sf::Vector2f size, sf::Color color)
+    {
+        setSize(size);
+        setFillColor(color);
+        setPosition(position);
+    }
+
+    void moveBullet(sf::RenderWindow& window)
+    {
+        velocity.x = 0;
+        velocity.y = 0;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+        {
+            velocity.y = -speed;
+            if (getPosition().y <= 0)
+            {
+                setPosition(getPosition().x, 0);
+            }
+        }
+    }
+    void update(float deltaTime)
+    {
+        move(velocity.x * deltaTime, velocity.y * deltaTime);
+    }
+    
+    void draw(sf::RenderWindow& window)
+    {
+        window.draw(*this);
+    }   
+
+private:
+    sf::Vector2f velocity;
+    float speed = 400.0f;
+};
+
+
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(), "Screen", sf::Style::Fullscreen);
+    window.setFramerateLimit(60);
+    
+    sf::Texture backgroundTexture;
+    if(!backgroundTexture.loadFromFile("background.png"))
+    {
+        std::cout << "Error loading background texture" << std::endl;
+    }
+    sf::Sprite background;
+    background.setTexture(backgroundTexture);
+    background.setScale(2, 2);
+    
+    Player player(sf::Vector2f(100, 100), sf::Vector2f(5, 5));
+    sf::Texture texture;
+    if(!texture.loadFromFile("player.png"))
+    {
+        std::cout << "Error loading player texture" << std::endl;
+    }
+    player.setTexture(texture);
+    
+    
+    
+    std::vector<sf::CircleShape> bullets;
 
-    sf::Vector2f velocity(0.0f, 0.0f);
     sf::Clock clock;
-    float speed = 400.0f;
 
-    sf::RectangleShape player;
-    player.setSize(sf::Vector2f(30, 30));
-    player.setFillColor(sf::Color::Blue);
-    player.setPosition(100, 100);
-    
-    
     while (window.isOpen())
     {
         sf::Event event;
@@ -23,50 +152,34 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
         {
-            velocity.x = -speed;
-            if(player.getPosition().x <= 0)
-            {
-                player.setPosition(0, player.getPosition().y);
-            }
+            window.close();
         }
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {    
-            velocity.x = speed;
-            if(player.getPosition().x + player.getSize().x >= window.getSize().x)
-            {    
-                player.setPosition(window.getSize().x - player.getSize().x, player.getPosition().y);
-            }
-        }
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            velocity.y = -speed;
-            if(player.getPosition().y <= 0)
-            { 
-                player.setPosition(player.getPosition().x, 0);
-            }        
-        }
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            velocity.y = speed;
-            if(player.getPosition().y + player.getSize().y >= window.getSize().y)
-            {    
-                player.setPosition(player.getPosition().x, window.getSize().y - player.getSize().y);
-            }
-        }
-        else
-        {
-            velocity.x = 0;
-            velocity.y = 0; 
-        }
-
         float deltaTime = clock.restart().asSeconds();
-
+        
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            bullets.push_back(sf::CircleShape());
+            bullets.back().setRadius(5);
+            bullets.back().setOrigin(5, 5);
+            bullets.back().setPosition(player.getPosition()+sf::Vector2f(40, 0));
+        }
+        
+        
         window.clear();
-        player.move(velocity.x * deltaTime, velocity.y * deltaTime);
-        window.draw(player);
-        window.display();
+        window.draw(background);
+        player.draw(window);
+        player.movePlayer(window);
+        player.update(deltaTime);
+        
+        
+        for (int i = 0; i < bullets.size(); i++)
+        {
+            bullets[i].move(0, -5);
+            window.draw(bullets[i]);
+        }
+        
+        window.display();  
     }
 }
